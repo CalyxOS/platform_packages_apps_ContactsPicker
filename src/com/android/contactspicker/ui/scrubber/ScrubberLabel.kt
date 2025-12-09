e9e3f675a19e08943d09084d3b5991179b4dfa20
Bhavuk Garg <bhavukgarg@google.com>
2025-12-09 08:27:13 +0000

    Add ScrubberLabel composable
    Adds the `ScrubberLabel` composable to display the current section indicator next to the scrubber handle.
    The label's visibility is controlled by an optional `SectionKey`. The content of the label (a letter or icon)
    is derived from the provided key.
    
    Demo Video - http://shortn/_Qy9DIK72qP
    
    Bug: 441479813
    Test: atest ContactsPickerIntegrationTests ContactsPickerComponentTests
    Flag: android.content.flags.enable_system_contacts_picker
    Change-Id: Ibb7a3ea314aaacfbd75a59c25b021d45149c7410

---
+++ src/com/android/contactspicker/ui/scrubber/ScrubberLabel.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.contactspicker.ui.scrubber
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.android.contactspicker.ui.pickerscreen.SectionKey
+
+// TODO(b/468919056): Add FadeIn/FadeOut animation for ScrubberLabel
+/**
+ * Displays a label that shows the current section key during fast scrolling.
+ *
+ * This composable will be used with the scrubber composable. The content of the label is determined
+ * by the provided [sectionKey].
+ *
+ * @param sectionKey The current [SectionKey] to display. When not null, the label is visible and
+ *   shows content corresponding to the key type (e.g., a letter or an icon). When null, the label
+ *   is hidden.
+ */
+@Composable
+fun ScrubberLabel(sectionKey: SectionKey?) {
+    if (sectionKey != null) {
+        Surface(
+            shape = MaterialTheme.shapes.large,
+            color = MaterialTheme.colorScheme.surfaceContainerHighest,
+            modifier = Modifier.testTag(SCRUBBER_LABEL_TEST_TAG),
+            shadowElevation = 4.dp,
+        ) {
+            Box(
+                modifier = Modifier.padding(horizontal = 40.dp, vertical = 10.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                ScrubberContent(targetKey = sectionKey)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ScrubberContent(targetKey: SectionKey) {
+    when (targetKey) {
+        is SectionKey.LetterKey ->
+            Text(
+                text = targetKey.letter.toString(),
+                style = MaterialTheme.typography.displayLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.testTag(SCRUBBER_LABEL_TEXT_TEST_TAG),
+            )
+        is SectionKey.FavoriteIconKey ->
+            Image(
+                imageVector = targetKey.icon,
+                contentDescription = stringResource(targetKey.contentDescriptionRes),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                modifier = Modifier.size(56.dp).testTag(SCRUBBER_LABEL_FAVORITE_ICON_TEST_TAG),
+            )
+        is SectionKey.EmojiIconKey ->
+            Image(
+                imageVector = targetKey.icon,
+                contentDescription = stringResource(targetKey.contentDescriptionRes),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                modifier = Modifier.size(56.dp).testTag(SCRUBBER_LABEL_EMOJI_ICON_TEST_TAG),
+            )
+    }
+}
+
+internal const val SCRUBBER_LABEL_TEST_TAG = "scrubber_label"
+internal const val SCRUBBER_LABEL_TEXT_TEST_TAG = "scrubber_label_text"
+internal const val SCRUBBER_LABEL_FAVORITE_ICON_TEST_TAG = "scrubber_label_favorite_icon"
+internal const val SCRUBBER_LABEL_EMOJI_ICON_TEST_TAG = "scrubber_label_emoji_icon"
---
+++ tests/componenttests/src/com/android/contactspicker/ui/scrubber/ScrubberLabelTest.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.contactspicker.ui.scrubber
+
+import android.content.flags.Flags
+import android.platform.test.annotations.RequiresFlagsEnabled
+import android.platform.test.flag.junit.CheckFlagsRule
+import android.platform.test.flag.junit.DeviceFlagsValueProvider
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.contactspicker.ui.pickerscreen.SectionKey
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RequiresFlagsEnabled(Flags.FLAG_ENABLE_SYSTEM_CONTACTS_PICKER)
+@RunWith(AndroidJUnit4::class)
+class ScrubberLabelTest {
+
+    @get:Rule val checkFlagsRule: CheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule()
+    @get:Rule val composeTestRule = createComposeRule()
+
+    @Test
+    fun scrubberLabel_whenSectionKeyIsNull_nothingIsDisplayed() {
+        composeTestRule.setContent { ScrubberLabel(sectionKey = null) }
+        assertLabelContent(visibleTag = null)
+    }
+
+    @Test
+    fun scrubberLabel_whenSectionKeyIsLetter_displaysText() {
+        val letterKey = SectionKey.LetterKey('A')
+        composeTestRule.setContent { ScrubberLabel(sectionKey = letterKey) }
+        assertLabelContent(visibleTag = SCRUBBER_LABEL_TEXT_TEST_TAG)
+        composeTestRule.onNodeWithTag(SCRUBBER_LABEL_TEXT_TEST_TAG).assertTextEquals("A")
+    }
+
+    @Test
+    fun scrubberLabel_whenSectionKeyIsFavorite_displaysFavoriteIcon() {
+        val favoriteKey = SectionKey.FavoriteIconKey
+        composeTestRule.setContent { ScrubberLabel(sectionKey = favoriteKey) }
+        assertLabelContent(visibleTag = SCRUBBER_LABEL_FAVORITE_ICON_TEST_TAG)
+    }
+
+    @Test
+    fun scrubberLabel_whenSectionKeyIsEmoji_displaysEmojiIcon() {
+        val emojiKey = SectionKey.EmojiIconKey
+        composeTestRule.setContent { ScrubberLabel(sectionKey = emojiKey) }
+        assertLabelContent(visibleTag = SCRUBBER_LABEL_EMOJI_ICON_TEST_TAG)
+    }
+
+    /**
+     * Asserts the correct visibility of scrubber label content based on the provided visibleTag.
+     *
+     * @param visibleTag The test tag of the composable that should be visible. If null, asserts
+     *   that all content tags are absent.
+     */
+    private fun assertLabelContent(visibleTag: String?) {
+        val allTags =
+            listOf(
+                SCRUBBER_LABEL_TEXT_TEST_TAG,
+                SCRUBBER_LABEL_FAVORITE_ICON_TEST_TAG,
+                SCRUBBER_LABEL_EMOJI_ICON_TEST_TAG,
+            )
+
+        allTags.forEach { tag ->
+            if (tag == visibleTag) {
+                composeTestRule.onNodeWithTag(tag).assertIsDisplayed()
+            } else {
+                composeTestRule.onNodeWithTag(tag).assertDoesNotExist()
+            }
+        }
+    }
+}
