c87f641d124b2464272feeef49b701b2b836b94a
Olivier <onshimiye@google.com>
2026-03-18 13:51:41 +0000

    Persist search text on rotation
    Before: http://shortn/_EYrx4a1c7S
    After: http://shortn/_Jh1dFXM5tO
    
    Bug: 480040590
    Test: atest ContactsPickerComponentTests ContactsPickerIntegrationTests
    Test: manual
    Flag: android.content.flags.enable_system_contacts_picker
    Change-Id: I978376cf9100d1a252aed91fada082d8816a8cf0

--- src/com/android/contactspicker/ui/pickerscreen/ContactsPickerSearchBar.kt
+++ src/com/android/contactspicker/ui/pickerscreen/ContactsPickerSearchBar.kt
@@ -42,7 +42,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -75,7 +75,7 @@ fun ContactsPickerSearchBar(
     onExitSearch: () -> Unit,
 ) {
 
-    var query by remember { mutableStateOf("") }
+    var query by rememberSaveable { mutableStateOf("") }
     val keyboardController = LocalSoftwareKeyboardController.current
 
     SearchBar(
